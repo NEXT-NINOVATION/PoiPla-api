9adb9d6f4e926acf2164396529c0e02c319c3fc5
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2022-05-27 14:35:48 +0900

    [change]

--- laravel/database/migrations/2014_10_12_000000_create_users_table.php
+++ laravel/database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,9 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            
+            // Firebaseから発行されたUIDを関連づけるためのUID
+            $table->string('uid')->unique();
             $table->rememberToken();
             $table->timestamps();
         });
