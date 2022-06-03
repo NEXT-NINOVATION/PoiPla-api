7d542f47eed4347e757b6992a9c141e12e0ab805
yumekiti <yumekiti120440@gmail.com>
2022-06-03 16:35:12 +0900

    `add` sanctum

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -14,6 +14,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+// 認証
+Route::get('/register', [UserController::class, 'register']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/test', function () {
+        return 'ok';
+    });
 });
