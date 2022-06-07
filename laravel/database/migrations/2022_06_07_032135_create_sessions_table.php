2ac171a227552211c58969f2cf43ffaa45ff4b3c
yumekiti <yumekiti120440@gmail.com>
2022-06-07 12:25:55 +0900

    `add` sessions table

---
+++ laravel/database/migrations/2022_06_07_032135_create_sessions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sessions', function (Blueprint $table) {
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('dust_box_id');
+            $table->date('completed_at');
+            $table->date('complete_flag');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+            // $table->foreign('dust_box_id')->references('id')->on('dust_boxes');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sessions');
+    }
+};
