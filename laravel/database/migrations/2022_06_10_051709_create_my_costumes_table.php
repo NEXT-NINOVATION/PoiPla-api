2046302a3a065f8c98d274bc6095593a4052069f
yumekiti <yumekiti120440@gmail.com>
2022-06-10 14:37:20 +0900

    `add` my_costumes table

---
+++ laravel/database/migrations/2022_06_10_051709_create_my_costumes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('my_costumes', function (Blueprint $table) {
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('costume_id');
+            $table->timestamps();
+
+            // $table->foreign('user_id')->references('id')->on('users');
+            // $table->foreign('costume_id')->references('id')->on('costume');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('my_costumes');
+    }
+};
