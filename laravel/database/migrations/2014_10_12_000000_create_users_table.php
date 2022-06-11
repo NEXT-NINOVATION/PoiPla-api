1ffd3de059e25aaef51f515b7dfcc579e5781e8d
yumekiti <yumekiti120440@gmail.com>
2022-06-11 20:33:48 +0900

    `chenge` users table

--- laravel/database/migrations/2014_10_12_000000_create_users_table.php
+++ laravel/database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,14 +14,14 @@ return new class extends Migration {
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('costume_id')->default(0);
+            $table->unsignedBigInteger('costume_id');
             $table->integer('level')->default(1);
             $table->integer('exp')->default(0);
             $table->integer('point')->default(0);
             $table->integer('total_pet')->default(0);
             $table->timestamps();
 
-            // $table->foreign('costume_id')->references('id')->on('costume');
+            $table->foreign('costume_id')->references('id')->on('costumes');
         });
     }
 
