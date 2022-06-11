796cda5e30afe0c5fd9bee107848da61ecda7070
yumekiti <yumekiti120440@gmail.com>
2022-06-11 22:08:51 +0900

    `chenge` my_costumes table

--- laravel/database/migrations/2022_06_10_051709_create_my_costumes_table.php
+++ laravel/database/migrations/2022_06_10_051709_create_my_costumes_table.php
@@ -17,8 +17,8 @@ return new class extends Migration {
             $table->unsignedBigInteger('costume_id');
             $table->timestamps();
 
-            // $table->foreign('user_id')->references('id')->on('users');
-            // $table->foreign('costume_id')->references('id')->on('costume');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('costume_id')->references('id')->on('costumes');
         });
     }
 
