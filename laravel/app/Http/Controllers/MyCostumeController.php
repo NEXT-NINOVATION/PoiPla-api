6bd3a074c5af285af70fb91aa81f2426865db228
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2022-06-28 11:18:44 +0900

    feat: 保有しているコスチュームを取得できるようにしました

---
+++ laravel/app/Http/Controllers/MyCostumeController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+use App\Models\User;
+use App\Models\Costume;
+class MyCostumeController extends Controller
+{
+
+    public function index()
+    {
+        $user = Auth::user();
+        return $user->costumes()->get();
+    }
+
+    public function attachAllCostumesIntoAllUsers()
+    {
+
+        $users = User::with('costumes')->get();
+        $costumes = Costume::get();
+        foreach ($costumes as $costume)
+        {
+            foreach ($users as $user)
+            {
+                if (!$user->costumes || !$user->costumes->count()){
+                    $user->costumes()->attach($costume->id);
+                }
+            }
+        }
+
+        return [
+            'users' => User::with('costumes')->get(),
+            'costumes' => $costumes,
+        ];
+        
+    }
+}
