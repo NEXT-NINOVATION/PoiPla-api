cbb2d23c3fba1924430c7f27049be2f978b66da8
yumekiti <yumekiti120440@gmail.com>
2022-06-30 15:02:44 +0900

    `add` comment

--- laravel/app/Http/Controllers/SessionController.php
+++ laravel/app/Http/Controllers/SessionController.php
@@ -33,11 +33,11 @@ class SessionController extends Controller
         ]);
     }
 
-    // public function pushes(Request $request)
-    public function pushes()
+    // ゴミを入れた時の処理
+    public function pushes(Request $request)
     {
-        // $token = $request->input("token");
-        $token = "hoge";
+        // トークン取得
+        $token = $request->input("token");
         $dust_box = DustBox::where("token", $token)->firstOrFail();
 
         $session = $dust_box->sessions()->first();
@@ -49,9 +49,9 @@ class SessionController extends Controller
         // ガチャ結果がハズレなら
         if (!$clatter) {
             $result = ["earn_exp" => config("app.clatter_earn_exp", 10)];
-            // デバックしにくいのでコメントアウト
-            // 時間以内でかつuser_idが一致しているか
-            // }elseif($completed_at->gte(new Carbon('now')) && $session->user_id == $user_id){
+        // デバックしにくいのでコメントアウト
+        // 時間以内でかつuser_idが一致しているか
+        // }elseif($completed_at->gte(new Carbon('now')) && $session->user_id == $user_id){
         } elseif ($session->user_id == $user_id) {
             $result = ClatterResult::create([
                 "session_id" => $session->id,
@@ -60,6 +60,7 @@ class SessionController extends Controller
             ]);
         }
 
+        // 同じセッション内で捨てたゴミの数を取得してイベント発火する
         event(
             new MyEvent(
                 $dust_box,
@@ -71,6 +72,7 @@ class SessionController extends Controller
         return [$session, $result];
     }
 
+    // セッション終了させる
     public function complete($dustBoxId, $sessionId)
     {
         $dustBox = DustBox::findOrFail($dustBoxId);
